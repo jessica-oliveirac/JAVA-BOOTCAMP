06fb98b63af1c7501294e4b260248ecfe28c2711
jessica-oliveirac <jssicac.deoliveira@yahoo.com.br>
2023-03-21 01:14:01 -0300

    Upload na pasta metodos

---
+++ JAVA-INTRO/metodos/src/ex2/Main.java
@@ -0,0 +1,13 @@
+package ex2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Exercicio Quadrilatero");
+		Quadrilatero.area(3);
+		Quadrilatero.area(5d,5d);//ou 5,5
+		Quadrilatero.area(7,8,9);
+		Quadrilatero.area(5f,5f);//ou 5,5
+	}
+
+}
