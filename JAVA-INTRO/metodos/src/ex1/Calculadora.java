c2e4de37048fb2af4674f873e8faeb808edea723
jessica-oliveirac <jssicac.deoliveira@yahoo.com.br>
2023-03-20 23:25:51 -0300

    "Upload de arquivos de Programação Orientada a Objetos"

---
+++ JAVA-INTRO/metodos/src/ex1/Calculadora.java
@@ -0,0 +1,23 @@
+package ex1;
+
+public class Calculadora {
+	public static void soma(double numero1, double numero2){
+		double resultado = numero1 + numero2;
+		System.out.println("A soma de " + numero1 + " mais " + numero2 + " e " + resultado);
+	}
+	
+	public static void subtracao(double numero1, double numero2) {
+		double resultado = numero1 - numero2;
+		System.out.println("A soma de " + numero1 + " mais " + numero2 + " e " + resultado);
+	}
+	
+	public static void multiplicacao(double numero1, double numero2) {
+		double resultado = numero1 * numero2;
+		System.out.println("A soma de " + numero1 + " mais " + numero2 + " e " + resultado);
+	}
+	
+	public static void divisao (double numero1, double numero2) {
+		double resultado = numero1 / numero2;
+		System.out.println("A soma de " + numero1 + " mais " + numero2 + " e " + resultado);
+	}
+}
