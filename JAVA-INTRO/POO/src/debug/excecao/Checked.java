7e6bc3b49f051c8194f460721da2903d8eea00cc
jessica-oliveirac <jssicac.deoliveira@yahoo.com.br>
2023-05-20 18:42:54 -0300

    Subindo arquivo de exceções

---
+++ JAVA-INTRO/POO/src/debug/excecao/Checked.java
@@ -0,0 +1,34 @@
+package debug.excecao;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Checked {
+	public static void main(String[] args) throws IOException {
+		String nomeDoArquivo = "romances-blake-crouch.txt";
+		imprimirArquivoNoConsole( nomeDoArquivo);
+		
+		System.out.println("Apesar da exception ou não, o programa continua...");
+	}
+	
+	public static void imprimirArquivoNoConsole(String nomeDoArquivo) throws IOException {
+		File file = new File(nomeDoArquivo);
+		
+		BufferedReader br = new BufferedReader(new FileReader(file.getName()));
+		String line = br.readLine();
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		do {
+			bw.write(line);
+			bw.newLine();
+			line=br.readLine();
+		} while(line != null);
+		bw.flush();	//flush:descarregar
+		br.close();
+	}
+}
