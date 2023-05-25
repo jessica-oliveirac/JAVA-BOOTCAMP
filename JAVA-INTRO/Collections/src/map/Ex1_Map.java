d99b8ee513db490f9a2cd45b4564f6e69093e767
jessica-oliveirac <jssicac.deoliveira@yahoo.com.br>
2023-05-24 23:31:36 -0300

    Atualizando Collections

---
+++ JAVA-INTRO/Collections/src/map/Ex1_Map.java
@@ -0,0 +1,10 @@
+package map;
+
+public class Ex1_Map {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JAVA-INTRO/Collections/src/set/Ex1_Set.java
@@ -0,0 +1,10 @@
+package set;
+
+public class Ex1_Set {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
